bb3f5fa1d8480762eed9c603cee18614e2aa814f
DavidGtzFrc19 <89830163+DavidGtzFrc19@users.noreply.github.com>
2021-08-31 01:18:19 -0500

    Add files via upload

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,15 @@
+import unittest
 
+from index import temperature
+
+class TestTemperatura(unittest.TestCase):
+
+    def test_grados_centigrados(self):
+        temp_k = 200
+
+        temp_c = (temp_k - 27)
+
+        self.assertEqual(temp_c, 173)
+        
+if __name__ == '__main__':
+    unittest.main()
